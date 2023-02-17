ba2766f3fb1487b03467e375e340528936b606a6
kedi1991 <okurut.henry@yahoo.com>
2023-02-17 02:21:45 +0000

    fix the error with context

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
 import os
-from taskmanager import app
+from taskmanager import app, db
+
 
 if __name__ == "__main__":
+
+    def create_tables(self):
+        with app.app_context():
+            db.create_all()
+    create_tables(db)
     app.run(
         host=os.environ.get("IP"), 
         port=os.environ.get("PORT"),
         debug=os.environ.get("DEBUG")
     )
+
+    
+
+    
--- taskmanager/models.py
+++ taskmanager/models.py
@@ -3,8 +3,8 @@ from taskmanager import db
 
 class Category(db.Model):
     # schema for the category model
-    id = db.column(db.Integer, primary_key=True)
-    category_name = db.column(db.String(25), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship(
         "Task", backref="category", cascade="all, delete", lazy=True)
 
@@ -15,13 +15,13 @@ class Category(db.Model):
 
 class Task(db.Model):
     # schema for the task model
-    id = db.column(db.Integer, primary_key=True)
-    task_name = db.column(db.String(50), unique=True, nullable=False)
-    task_description = db.column(db.Text, nullable=false)
-    is_urgent = db.column(db.Boolean, default=False, nullable=False)
-    due_date = db.column(db.Date, nullable=False)
-    category_id = db.column(db.integer, db.ForeignKey(
-        "category.id", ondelete="CASCADE", nullable=False))
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        "category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
